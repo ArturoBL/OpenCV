15711f8320f0a54ad3e0d72559b7ec1bce94c0d8
ArturoBL <67767374+ArturoBL@users.noreply.github.com>
2021-05-15 03:20:16 -0500

    Corregido Proyecto PlayVideo

--- Basic/PlayVideo/main.py
+++ Basic/PlayVideo/main.py
@@ -1,14 +1,15 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('..\Media\video.mp4')
+cap = cv2.VideoCapture('../../Media/video.mp4')
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    img = cv2.resize(frame,(640,360),interpolation = cv2.INTER_LANCZOS4)
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',img)
+    if ret:
+        cv2.imshow('frame',frame)
+    else:
+        break
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
