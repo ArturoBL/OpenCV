64deffc47f8031c09ef0ce8ba1940bd5cbea8a0c
ArturoBL <67767374+ArturoBL@users.noreply.github.com>
2021-05-29 01:20:13 -0500

    Se Agregaron comentarios a PlayVideo

--- Basic/PlayVideo/PlayVideo.py
+++ Basic/PlayVideo/PlayVideo.py
@@ -1,17 +1,21 @@
-import numpy as np
 import cv2
 
+#Abrimos stream de video desde archivo
 cap = cv2.VideoCapture('../../Media/video.mp4')
 
+#Ciclo para leer cada frame de video
 while(cap.isOpened()):
+    #Leemos frame
     ret, frame = cap.read()
 
+    #Si se obtiene frame se muestra en ventana con tìtulo frame
     if ret:
-        cv2.imshow('frame',frame)
+        cv2.imshow('Frame',frame)
     else:
         break
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
+#Liberamos stream
 cap.release()
 cv2.destroyAllWindows()
