d53eb3cb28bd2391335e9a17cbab8021c8cf3880
Arturo Beristain López <dark.laker@gmail.com>
2022-09-18 13:10:26 -0500

    Imshow Agregado e instrucciones instalación de VSCode OpenCV Cuda

---
+++ C++/ImShow/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int, char**) {
+    std::cout << "Starting OpenCV project!\n";
+    auto filename = "c:/temp/lena.jpg";
+    auto image = cv::imread(filename);
+
+    cv::imshow("image",image);
+    cv::waitKey();
+}
